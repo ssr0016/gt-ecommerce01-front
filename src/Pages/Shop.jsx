c0e89986d1f50c8c8999945efb7d6d0cfbb51a47
ssr0016 <samsonrecluta0016@gmail.com>
2026-03-12 20:46:44 +0800

    create components for Hero, Item, Popular and Offers

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,5 +1,15 @@
+import Hero from "../Components/Hero/Hero";
+import Offers from "../Components/Offers/Offers";
+import Popular from "../Components/Popular/Popular";
+
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+    </div>
+  );
 };
 
 export default Shop;
