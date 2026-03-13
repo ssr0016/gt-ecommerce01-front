0a1ff65a026b15cec2604091090940ee23ad731b
ssr0016 <samsonrecluta0016@gmail.com>
2026-03-13 10:16:39 +0800

    add Components - Footer, NewCollections, NewsLetter, ShopCategory for mens, womens and kids

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,4 +1,6 @@
 import Hero from "../Components/Hero/Hero";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
@@ -8,6 +10,8 @@ const Shop = () => {
       <Hero />
       <Popular />
       <Offers />
+      <NewCollections />
+      <NewsLetter />
     </div>
   );
 };
